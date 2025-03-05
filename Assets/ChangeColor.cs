bdbebc0c0944754abe41805e10421f771896edee
Laura Ruiz <andrea.ruiz.f@hotmail.com>
2025-03-04 22:18:58 -0600

    Cambio de color fijo
    Se cambia funcionalidad para cambiar de color en un arreglo de colores fijo.

--- Assets/ChangeColor.cs
+++ Assets/ChangeColor.cs
@@ -1,25 +1,34 @@
+using System.Drawing;
 using Unity.VisualScripting;
 using UnityEngine;
+using Color = UnityEngine.Color;
 
 public class ChangeColor1 : MonoBehaviour
 {
     public GameObject model;
-    public Color color;
+    //public Color color;
+    public Color[] colors = { Color.cyan, Color.green, Color.red, Color.yellow, Color.gray };
     public Material colorMaterial;
+    public int i = 0;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        colorMaterial.color = colors[i];
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
     public void ChangeColor_BTN()
     {
-        model.GetComponent<Renderer>().material.color = color;
-        colorMaterial.color = color;
+        //model.GetComponent<Renderer>().material.color = color;
+        //colorMaterial.color = color;
+        if (i < 4)
+            i++;
+        else
+            i = 0;
+        colorMaterial.color = colors[i];
     }
 }
